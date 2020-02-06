7ea217219fe75e57a1112f4d9c97afd34a2357a9
NielsFerreira <59962561+NielsFerreira@users.noreply.github.com>
2020-02-06 15:28:36 +0100

    Choix par nom

--- TP3/explo.c
+++ TP3/explo.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(){
 	
 	printf("Bonjour aventurier !\n");
 	
-	int destination;
+	char destination[100];
+	char destination1[100] = "Velika";
+	char destination2[100] = "Hautegarde";
 	
-	printf("\n Tapez '1' pour aller a Velika ou tapez '2' pour aller a Hautegarde. \n");
 	
-	scanf("%d",&destination);
+	printf("\n Choisissez votre destination : \n");
 	
-	if(destination == 1){
+	printf(" -Velika :\n");
+	printf(" -Hautegarde :\n");
+	
+	scanf("%s",&destination);
+	
+	if(strcmp(destination, destination1)==0){
+		
 		printf("En avant toute pour Velika !");
 		
 		return 0;
 	}else
-		if(destination == 2){
+		if(strcmp(destination, destination2)==0){
+			
 		printf("Direction Hautegarde !");
 		
 		return 0;
+	}else
+		if(strcmp(destination, destination1)==1){
+			if(strcmp(destination, destination2)==1){
+				
+				printf("Vous ne pouvez pas aller par la.\n");
+		
+		}
 	}
 }
