e6ac3e66300f7542032eac31251e3507c924923b
NielsFerreira <59962561+NielsFerreira@users.noreply.github.com>
2020-02-06 13:59:16 +0100

    Exo 1, question 3
    fin de la question

--- TP3/explo.c
+++ TP3/explo.c
@@ -4,7 +4,22 @@
 
 int main(){
 	
-	printf("beginning");
+	printf("Bonjour aventurier !\n");
 	
+	int destination;
 	
+	printf("\n Tapez '1' pour aller a Velika ou tapez '2' pour aller a Hautegarde \n");
+	
+	scanf("%d",&destination);
+	
+	if(destination == 1){
+		printf("En avant toute pour Velika !");
+		
+		return 0;
+	}else
+		if(destination == 2){
+		printf("Direction Hautegarde !");
+		
+		return 0;
+	}
 }
